583e1c00b032352e5e03484a6383b324af905d99
the-ianyu <yuy8@stconline.edu.hk>
2023-03-16 22:38:29 +0800

    2023.3.16.0.2
    + 2015 day 10

---
+++ 2015/10. Elves Look, Elves Say/elves-look-elves-say-combined.py
@@ -0,0 +1,18 @@
+import os
+from itertools import groupby
+
+filename = "elves-look-elves-say.txt"
+here = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(here, filename)
+
+with open(filepath, "r") as f:
+    content = f.read()
+
+def looksay(line):
+    return "".join(str(len(list(y)))+str(x) for x, y in groupby(line))
+
+for i in range(50):
+    content = looksay(content)
+    if i == 39:
+        print("Part 1:", len(content))
+print("Part 2:", len(content))
