4a3c0a3a4e26fe0cd2cba3bfa84a991825799616
the-ianyu <yuy8@stconline.edu.hk>
2023-10-29 16:12:31 +0800

    2020-10-2 completion [19]

--- 2020/10. Adapter Array/adapter-array-2.py
+++ 2020/10. Adapter Array/adapter-array-2.py
@@ -1,31 +1,16 @@
-# import os, functools
+import os
 
-# filename = "adapter-array.txt"
-# here = os.path.dirname(os.path.abspath(__file__))
-# filepath = os.path.join(here, filename)
+filename = "adapter-array.txt"
+here = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(here, filename)
 
-# with open(filepath, "r") as f:
-#     content = sorted([int(x) for x in f.read().splitlines()])
-# content = [0] + content + [content[-1]+3]
-# content = tuple(content)
-# print(content)
-# # content = (0, 1, 4, 6, 7, 9, 12) #testcase
+with open(filepath, "r") as f:
+    content = sorted([[int(x), 0] for x in f.read().splitlines()], key=lambda x: x[0])
 
-# def getNeighbors(node):
-#     global content
-#     neighbours = []
-#     for i in range(1, 4):
-#         if node+i in content:
-#             neighbours.append(node+i)
-#     return tuple(neighbours)
-# @functools.lru_cache(maxsize=None)
-# def nodesearch(node):
-#     global content
-#     total = 0
-#     neighbors = getNeighbors(node)
-#     total += len(neighbors)
-#     for x in neighbors:
-#         total += nodesearch(x)
-#     return total
+content.insert(0, [0, 1])
+for i in range(1, len(content)):
+    for j in range(min(i, 3)):
+        if content[i][0] - content[i-j-1][0] <= 3:
+            content[i][1] += content[i-j-1][1]
 
-# print(nodesearch(0))
+print("Part 2:", content[-1][1])
