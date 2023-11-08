c619f954677a2d2fbcd89fb6bc7bf06b042fcef2
the-ianyu <yuy8@stconline.edu.hk>
2023-11-08 11:37:12 +0800

    2021-6 completion [21]

---
+++ 2021/6. Lanternfish/lanternfish-2.py
@@ -0,0 +1,18 @@
+import os
+
+filename = "lanternfish.txt"
+here = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(here, filename)
+
+with open(filepath, "r") as f:
+    content = [int(x) for x in f.read().split(",")]
+
+total = len(content)
+days = 256
+for i in content:
+    c = [0 for _ in range(9)]
+    c[i] = 1
+    for j in range(days):
+        total += c[0]
+        c = [c[1], c[2], c[3], c[4], c[5], c[6], c[0]+c[7], c[8], c[0]]
+print("Part 2:", total)
