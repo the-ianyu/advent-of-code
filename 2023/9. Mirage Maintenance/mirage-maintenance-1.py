98ad4aa9d939c4abf77f1da01b60a67a04eed34a
the-ianyu <yuy8@stconline.edu.hk>
2023-12-09 13:43:03 +0800

    2023-9 completion [31]

---
+++ 2023/9. Mirage Maintenance/mirage-maintenance-1.py
@@ -0,0 +1,16 @@
+from os import path
+
+filename = "mirage-maintenance.txt"
+here = path.dirname(path.abspath(__file__))
+filepath = path.join(here, filename)
+
+with open(filepath, "r") as f:
+    content = f.read().splitlines()
+
+def next_term(polynomial):
+    sequences = [polynomial]
+    while len(sequences[-1]) > 1:
+        sequences.append([sequences[-1][i]-sequences[-1][i-1] for i in range(1, len(sequences[-1]))])
+    return sum([x[-1] for x in sequences])
+
+print("Part 1:", sum([next_term([int(x) for x in line.split()]) for line in content]))
